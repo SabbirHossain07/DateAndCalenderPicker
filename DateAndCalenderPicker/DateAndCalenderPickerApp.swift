22d26d9d7a048b434286fb2eb7886c9b09d0adec
Sabbir Hossain <sabbirsopnil07@gmail.com>
2025-12-15 12:13:06 +0600

    Initial Commit

---
+++ DateAndCalenderPicker/DateAndCalenderPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateAndCalenderPickerApp.swift
+//  DateAndCalenderPicker
+//
+//  Created by Sopnil Sohan on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DateAndCalenderPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
